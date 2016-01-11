50224a8811cf6ff1e4bef2a2db55f7397df272b9
Dustin Davis and Mollie Bootsma <mollie.bootsma@gmail.com>
2016-01-11 11:31:21 -0800

    Adding initial files and directories

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("WUtang Killa Bees in your dome.");
+  });
+
+  $("p").dblclick(function() {
+    alert("WUtang Killa Bees in your dome.");
+  });
+
+  $(".clickable").click(function() {
+    $(.pansy).toggle();
+  });
+})
